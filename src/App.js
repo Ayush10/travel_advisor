3c54f682fdaf242eb4daa0df5e52ac3fc9c09c26
Ayush10 <ayushgreat10@gmail.com>
2022-12-08 00:48:17 +0545

    Final

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { CssBaseline, Grid} from '@material-ui/core';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+    
+    return (
+        <div>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{width: ''100%'}}>
+            </Grid>
+        </div>
+    );
+}
+
+export default App;
